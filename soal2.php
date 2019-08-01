cfeaad10552f81b9771ead4d8bfcfb5f61e905fe
farrelfdlhs <farrelsanan12@gmail.com>
2019-08-01 16:59:16 +0700

    Perubahan Rumus

--- soal2.php
+++ soal2.php
@@ -1,12 +1,23 @@
 <?php
 
-    $tama = 32;
-    $toni = 32/2;
-    $tini = 32-14;
+    $JTama = 1;
+    $JToni = 0.5;
+    $JTinitama = 14;
+    $Jtinitoni = 34;
 
-    echo "Umur Tama : $tama <br>";
-    echo "umur Toni : $toni <br>";
-    echo "Umur Tini : $tini ";
-
-
+    echo "Umur Tama = $JTama X<br>";
+    echo "Umur Toni = $JToni X<br>";
+    echo "Umur Toni + Umur Tini = $Jtinitoni<br>";
+    echo "$JToni + $JTama X - $JTinitama = $Jtinitoni<br>";
+    $x = $JTama + $JToni;
+    $y = $JTinitama + $Jtinitoni;
+    $z = $y/$x;
+    echo "$x X=$JTinitama+$Jtinitoni=$y<br>";
+    echo "$x X = $y<br>";
+    echo "X = $y : $x=$z<br>";
+    echo "Umur Tama = $z<br>";
+    $tini = $z-$JTinitama;
+    echo "Umur Tini = $tini<br>";
+    $toni = $z/2;
+    echo "Umur Toni = $toni";
 ?>
